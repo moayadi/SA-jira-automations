07f4b0c64c5c3df482ef23b722743dbd5d96822d
moayad <moayad@hashicorp.com>
2024-09-03 12:33:54 +0800

    version update test

--- sa-logwork.user.js
+++ sa-logwork.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         sa-logwork-jira
 // @namespace    http://tampermonkey.net/
-// @version      1.2
+// @version      1.3
 // @description  Inject an inline expansion for data entry to update work description in Jira, with template management, clear functionality, activity summary, and total hours input
 // @author       Moayad Ismail
 // @match        https://hashicorp.atlassian.net/*
